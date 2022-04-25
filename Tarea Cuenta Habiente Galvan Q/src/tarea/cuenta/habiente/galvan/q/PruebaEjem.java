d0d1ae313d6475d6fb1635057e0037bdc5e0bc93
HecGalvQ <hectorquiroz72@aragon.unam.mx>
2022-04-25 11:29:36 -0500

    Tarea Cuenta Habiente

---
+++ Tarea Cuenta Habiente Galvan Q/src/tarea/cuenta/habiente/galvan/q/PruebaEjem.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea.cuenta.habiente.galvan.q;
+
+/**
+ *
+ * @author herma
+ */
+public class PruebaEjem {
+     public static void main(String args[]){
+         
+        CuentaHabiente[] cuentas = new CuentaHabiente[5];   
+        cuentas[0]=new CuentaHabiente("HEcGA08033","Héctor Galvan", 75000);
+        cuentas[1]=new CuentaHabiente("Ficc10084","Filiberto Castillo", 41500);
+        cuentas[2]=new CuentaHabiente("JaGc09124","Jonathan Giron", 56000);
+        cuentas[3]=new CuentaHabiente("Vach05012","Vanessa Hernandez", 49999);
+        cuentas[4]=new CuentaHabiente("Mass02901","Miguel Sainz", 50025);
+        
+        for (int i = 0; i < cuentas.length; i++) {
+            
+            cuentas[i].retirarDinero(10000);
+            System.out.println(cuentas[i]);
+            if(cuentas[i].getBalance()<=50000){
+                System.out.println("La cuenta de " +cuentas[i].nombre);
+                System.out.println(" es REGULAR");
+            }else{
+                System.out.println("La cuenta de " +cuentas[i].nombre);
+                System.out.println(" es PREMIUM");               
+            }
+        }
+}
+}
+    
+
